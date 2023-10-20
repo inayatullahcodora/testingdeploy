4a98a32b30e7798c1612ad2ab1dbae30707bb37b
inayat ullah <inayatullah@codora.io>
2023-10-20 16:03:50 +0500

    testing deployment

--- src/App.js
+++ src/App.js
@@ -5,9 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit  Xyz<code>src/App.js</code> and save to reload.
+          testing app deployment
         </p>
         <a
           className="App-link"
